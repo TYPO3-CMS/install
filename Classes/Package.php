e75e4458f12e56c5739cf733e5cfb53f1c90c12a
Thomas Maroschik <tmaroschik@dfau.de>
2013-07-08 21:22:31 +0200

    [FEATURE] Integrate preliminary PackageManager API
    The Package Manager from Flow has been backported and integrated as
    CMS Package Manager. This enables us to use full vendor namespaces for
    extensions and brings us closer to a unified TYPO3 package format.
    
    For more Details see: http://wiki.typo3.org/Blueprints/Packagemanager
    
    Resolves: #47018
    Releases: 6.2
    Change-Id: I52abedaf470cebddb3553e37581021bf368380e6
    Reviewed-on: https://review.typo3.org/19605
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn
    Reviewed-by: Alexander Opitz
    Reviewed-by: Sascha Egerer
    Reviewed-by: Thomas Maroschik
    Tested-by: Thomas Maroschik
    Tested-by: Alexander Opitz
    Reviewed-by: Ernesto Baschny
    Tested-by: Ernesto Baschny

--- Classes/Package.php
+++ Classes/Package.php
@@ -1,10 +1,10 @@
 <?php
-namespace TYPO3\CMS\Install\Controller\Exception;
+namespace TYPO3\CMS\Install;
 
 /***************************************************************
  *  Copyright notice
  *
- *  (c) 2013 Christian Kuhn <lolli@schwarzbu.ch>
+ *  (c) 2013 Thomas Maroschik <tmaroschik@dfau.de>
  *  All rights reserved
  *
  *  This script is part of the TYPO3 project. The TYPO3 project is
@@ -15,6 +15,9 @@ namespace TYPO3\CMS\Install\Controller\Exception;
  *
  *  The GNU General Public License can be found at
  *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the textfile GPL.txt and important notices to the license
+ *  from the author is found in LICENSE.txt distributed with these scripts.
+ *
  *
  *  This script is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -24,9 +27,15 @@ namespace TYPO3\CMS\Install\Controller\Exception;
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
 
-/**
- * A controller exception thrown if a redirect loop is detected
- */
-class RedirectLoopException extends \TYPO3\CMS\Install\Exception {
+use TYPO3\CMS\Core\Package\Package as BasePackage;
 
+/**
+ * This is the install package
+ */
+class Package extends BasePackage {
+
+	/**
+	 * @var boolean
+	 */
+	protected $protected = TRUE;
 }
